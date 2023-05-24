a1faf62179ef27b0f8510ddedfaf70455cfe400b
Emma Lou Who <122497149+Emmah-lou@users.noreply.github.com>
2023-05-24 08:26:07 -0600

    updated active and remove buttons

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -1,7 +1,7 @@
 import "./requests";
 import $, { post } from "jquery";
 
-import { indexTasks, postTask } from "./requests.js";
+import { indexTasks, postTask, deleteTask, updateTask } from "./requests.js";
 
 $(document).ready(function () {
   // Load tasks on page load
@@ -12,17 +12,50 @@ $(document).ready(function () {
   // Function to display tasks
   function displayTasks(tasks) {
     var htmlString = tasks.map(function (task) {
+      var taskClass = task.completed ? "completed" : "active";
       return (
-        "<div class='col-12 mb-3 p-2 border rounded task' data-id='" +
+        "<div class='col-12 mb-3 p-2 border rounded task " +
+        taskClass +
+        "' data-id='" +
         task.id +
         "'>" +
         task.content +
+        "<button class='remove-button'>Remove</button>" +
+        "<button class='active-complete'>Active/Complete</button>" +
         "</div>"
       );
     });
 
     $("#tasks").html(htmlString);
   }
+  $(document).on("click", ".remove-button", function () {
+    var id = $(this).parent(".task").data("id");
+    deleteTask(
+      id,
+      function (response) {
+        console.log("Task deleted successfully:", response);
+        displayTasks(response.tasks);
+      },
+      function (error) {
+        console.error("Error deleting task:", error);
+      }
+    );
+  });
+
+  $(document).on("click", ".active-complete", function () {
+    var id = $(this).parent(".task").data("id");
+    //if task is active, mark as complete
+    updateTask(
+      id,
+      function (response) {
+        console.log("Task updated successfully:", response);
+        displayTasks(response.tasks);
+      },
+      function (error) {
+        console.error("Error updating task:", error);
+      }
+    );
+  });
 
   // Event listener for form submission
   $("#new_task").on("submit", function (event) {
@@ -41,33 +74,3 @@ $(document).ready(function () {
     );
   });
 });
-// $("#new_task").on("submit", function (event) {
-//   event.preventDefault();
-//   postTask($("#task_content").val(), function (response) {
-//     $("#tasks").append(
-//       "<div class='col-12 mb-3 p-2 border rounded task' data-id='" +
-//         response.task.id +
-//         "'> \
-//       " +
-//         response.task.content +
-//         "\
-//       </div>"
-//     );
-//   });
-// });
-
-// indexTasks(function (response) {
-//   var htmlString = response.tasks.map(function (task) {
-//     return (
-//       "<div class='col-12 mb-3 p-2 border rounded task' data-id='" +
-//       task.id +
-//       "'> \
-//       " +
-//       task.content +
-//       "\
-//       </div>"
-//     );
-//   });
-
-//   $("#tasks").html(htmlString);
-// });
--- app/javascript/src/requests.js
+++ app/javascript/src/requests.js
@@ -17,6 +17,32 @@ export var indexTasks = function (successCB, errorCB) {
   $.ajax(request);
 };
 
+export var deleteTask = function (id, successCB, errorCB) {
+  var request = {
+    type: "DELETE",
+    url: "api/tasks/" + id + "?api_key=1",
+    success: successCB,
+    error: errorCB,
+  };
+
+  $.ajax(request);
+};
+export var updateTask = function (id, successCB, errorCB) {
+  let url;
+  if ($(this).parent(".task").hasClass("active")) {
+    url = "api/tasks/" + id + "/mark_complete?api_key=1";
+  } else {
+    url = "api/tasks/" + id + "/mark_active?api_key=1";
+  }
+  var request = {
+    type: "PUT",
+    url: url,
+    success: successCB,
+    error: errorCB,
+  };
+  $.ajax(request);
+};
+
 export var postTask = function (content, successCB, errorCB) {
   var request = {
     type: "POST",
